47da5b74f216504d1456690dfb00c6383347be9b
ybbhosale2023 <144084673+ybbhosale2023@users.noreply.github.com>
2023-09-20 01:50:57 +0530

    Create my profile1.md

---
+++ my profile.m
@@ -0,0 +1,28 @@
+I. Introduction
+- Brief introduction of Yogesh Bhosale
+- Professional background and experience
+
+II. Experience with Salesforce
+- Extensive experience in Salesforce domain
+- Description of major components of Salesforce
+- Expertise in each component
+- Ability to build end-to-end solutions that meet business needs
+- Experience in creating custom applications and integrating them with other systems
+- Knowledge in data migration and lead flow management
+
+III. Other Areas of Expertise
+- Application development lifecycle management
+- Go-live support to ensure smooth transition to production phase
+- Data migration from legacy systems to new systems
+- Ensuring application security and integration with social media platforms
+- Managing lead flow across multiple industries
+
+IV. Work Ethics and Approach
+- Ability to understand problem process and constraints
+- Analyzing business situation and providing solutions
+- Breaking down solutions into smaller components and completing them timely
+- Unmatched level of expertise and maturity in consulting career
+- Taking minimum customers to provide more attention and big ROI
+
+V. Conclusion
+- Gratitude for considering Yogesh Bhosale for the project
